45ad06df30b64b5571825d2685298832a8040012
jzohdi <jzohdi@terpmail.umd.edu>
2019-01-04 15:11:57 -0500

    quotes load to specified height down on page

--- quotesTwo.js
+++ quotesTwo.js
@@ -1,14 +1,29 @@
 function randomIntRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+const replaceEach = (startString, searchArray) => {
+  let myString = startString;
+  searchArray.forEach(value => {
+    const reg = new RegExp(value[0], "g");
+    myString = myString.replace(reg, value[1]);
+  });
+  return myString;
+};
+
+const getRandomQuoteString = () => {
+  const randomIndex = randomIntRange(0, MAX_QUOTE_INDEX);
+  const newQuote = all_quotes[randomIndex];
+  return newQuote.author + "-- " + newQuote.quote + " --&nbsp;";
+};
 /*
  * TO DO work on smaller screen function
  * return var not direct math.round
  * amke screen selector smaller
  */
-var screenWidth = innerWidth;
-var screenHeight = innerHeight;
-var dimensions = [screenWidth, screenHeight];
+const screenWidth = innerWidth;
+const screenHeight = innerHeight;
+const dimensions = [screenWidth, screenHeight];
 // console.log(innerWidth)
 // console.log(innerHeight)
 
@@ -55,67 +70,61 @@ function getSize(leng) {
   }
 }
 
-var sizeToAdd;
-var pixelsDown = [0, 0];
-var countRows = 1;
+window.currentHeight = 0;
+const heightOfPage = window.innerHeight;
+const MAX_QUOTE_INDEX = all_quotes.length;
+window.countRows = 1;
 
-function loadQuotes(howManyRows) {
+const MOVE_RIGHT_CSS =
+  ".sizerowNum { font-size: sizeToAddpx;} @keyframes moveMe-rightrowNum { 0% { transform: translate(0, " +
+  "heightOnPagepx); } 100% { transform: translate(110%, heightOnPagepx); } }";
+const MOVE_LEFT_CSS =
+  ".sizerowNum { font-size: sizeToAddpx;} @keyframes moveMerowNum { 0% { transform: translate(0, " +
+  "heightOnPagepx); } 100% { transform: translate(-110%, heightOnPagepx); } }";
+
+function loadQuotes(stopHeight) {
   //   https://quotesondesign.com/api-v4-0/
   // var quoteAPI = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=?"
-  var totalRows = howManyRows;
-  console.log(all_quotes.length);
-  for (var currentRow = 1; currentRow <= totalRows; currentRow++) {
-    // if (quote.length <= 188 && quote.length >= 60) {
-    //   var heightOnPage = pixelsDown.pop();
-    //   sizeToAdd = getSize(quote.length);
-    //   // console.log(sizeToAdd);
-    //   // console.log(quote.length);
-    //   // console.log(sizeToAdd);
-    //   /*
-    //    * // TO DO replace if countrows odd or even with if ( $().hassClass ) then rows can move right or left randomly instead of every other
-    //    *    condence .toString() usage
-    //    */
-    //   if (countRows % 2 == 1) {
-    //     $("#css").append(
-    //       ".size" +
-    //         rowNum +
-    //         " { font-size: " +
-    //         sizeToAdd +
-    //         "px;} @keyframes moveMe-right" +
-    //         rowNum +
-    //         " { 0% { transform: translate(0, " +
-    //         heightOnPage +
-    //         "px); } 100% { transform: translate(110%, " +
-    //         heightOnPage +
-    //         "px); } }"
-    //     );
-    //     $("#row" + rowNum).html(quote);
-    //     // console.log(countRows.toString())
-    //     $("#row" + rowNum + "b").html(quote);
-    //   } else if (countRows % 2 == 0) {
-    //     $("#css").append(
-    //       ".size" +
-    //         rowNum +
-    //         " { font-size: " +
-    //         sizeToAdd +
-    //         "px;} @keyframes moveMe" +
-    //         rowNum +
-    //         " { 0% { transform: translate(0, " +
-    //         heightOnPage +
-    //         "px); } 100% { transform: translate(-110%, " +
-    //         heightOnPage +
-    //         "px); } }"
-    //     );
-    //     $("#row" + rowNum).html(quote);
-    //     // console.log(countRows.toString())
-    //     $("#row" + rowNum + "b").html(quote);
-    //   }
-    //   countRows += 1;
-    //   // pixelsDown += parseInt(sizeToAdd);
-    //   heightOnPage += parseInt(sizeToAdd);
-    //   pixelsDown.push(heightOnPage);
-    //   // console.log("row completed")
-    // } else {
+  // const totalRows = howManyRows;
+
+  // for (var currentRow = 1; currentRow <= totalRows; currentRow++)
+  while (currentHeight <= stopHeight + 20) {
+    initHTML(countRows);
+    initCSS(countRows);
+    init(countRows);
+    const newQuoteString = getRandomQuoteString();
+
+    const quoteLength = newQuoteString.length;
+
+    if (quoteLength <= 188 && quoteLength >= 60) {
+      const sizeToAdd = getSize(quoteLength);
+      console.log("currernt height: ", window.currentHeight);
+      if (window.countRows % 2 == 1) {
+        const cssText = replaceEach(MOVE_RIGHT_CSS, [
+          ["rowNum", countRows],
+          ["sizeToAdd", sizeToAdd],
+          ["heightOnPage", window.currentHeight]
+        ]);
+        $("#css").append(cssText);
+        $("#row" + countRows).html(newQuoteString);
+        //     // console.log(countRows.toString())
+        $("#row" + countRows + "b").html(newQuoteString);
+      } else if (window.countRows % 2 == 0) {
+        const cssText = replaceEach(MOVE_LEFT_CSS, [
+          ["rowNum", countRows],
+          ["sizeToAdd", sizeToAdd],
+          ["heightOnPage", window.currentHeight]
+        ]);
+        $("#css").append(cssText);
+        $("#row" + countRows).html(newQuoteString);
+        //     // console.log(countRows.toString())
+        $("#row" + countRows + "b").html(newQuoteString);
+      }
+      window.countRows += 1;
+
+      window.currentHeight += parseInt(sizeToAdd);
+    }
+    // else {
     //   loadQuotes(1);
     // }
     // var height = pixelsDown.pop();
@@ -130,93 +139,59 @@ function loadQuotes(howManyRows) {
  * TO DO add init for div elements, call next init with previous init
  *
  */
-function init(pageRows) {
-  for (var rowcount = 1; rowcount <= pageRows; rowcount++) {
-    var num = rowcount.toString();
-    var bodyDoc = $("#init" + num);
-    if (rowcount % 2 == 1) {
-      var html =
-        "<div class='container-fluid'><span id='row" +
-        num +
-        "' class='moveMe-right" +
-        num +
-        " size" +
-        num +
-        "'></span><span id='row" +
-        num +
-        "b' class='moveMe-right" +
-        num +
-        " moveTwo" +
-        num +
-        "-right size" +
-        num +
-        "'></span></div>";
-      bodyDoc.html(html);
-    } else {
-      var html =
-        "<div class='container-fluid'><span id='row" +
-        num +
-        "' class='moveMe" +
-        num +
-        " size" +
-        num +
-        "'></span><span id='row" +
-        num +
-        "b' class='moveMe" +
-        num +
-        " moveTwo" +
-        num +
-        " size" +
-        num +
-        "'></span></div>";
-      bodyDoc.html(html);
-    }
+const HTML_RIGHT =
+  "<div class='container-fluid'><span id='rownum' class='moveMe-rightnum sizenum'></span><span id='row" +
+  "numb' class='moveMe-rightnum moveTwonum-right sizenum'></span></div>";
+const HTML_LEFT =
+  "<div class='container-fluid'><span id='rownum' class='moveMenum sizenum'></span><span id='row" +
+  "numb' class='moveMenum  moveTwonum sizenum'></span></div>";
+
+function init(pageRow) {
+  // for (var rowcount = 1; rowcount <= pageRows; rowcount++) {
+  var num = pageRow.toString();
+  var bodyDoc = $("#init" + num);
+  if (pageRow % 2 == 1) {
+    const htmlForRight = replaceEach(HTML_RIGHT, [["num", num]]);
+    bodyDoc.html(htmlForRight);
+  } else {
+    const htmlForLeft = replaceEach(HTML_LEFT, [["num", num]]);
+    bodyDoc.html(htmlForLeft);
   }
-  loadQuotes(pageRows);
+  // }
 }
+const RIGHT_STYLE =
+  ".moveMe-rightnumb" +
+  " { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-right: 100%; animation: moveMe-right" +
+  "numb 20s infinite linear; position: absolute; right: 10%; } .moveTwonumb-right { animation-delay: 10s; }";
+const LEFT_STYLE =
+  ".moveMenumb" +
+  " { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-left: 100%; animation: moveMe" +
+  "numb 26s infinite linear; position: absolute; } .moveTwonumb{ animation-delay: 13s; }";
 
 function initCSS(styleNum) {
   var docStyle = $("#css");
-  for (var style = 1; style <= styleNum; style++) {
-    var numb = style.toString();
-    if (style % 2 == 1) {
-      var addStyle =
-        ".moveMe-right" +
-        numb +
-        " { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-right: 100%; animation: moveMe-right" +
-        numb +
-        " 20s infinite linear; position: absolute; right: 10%; } .moveTwo" +
-        numb +
-        "-right { animation-delay: 10s; }";
-      docStyle.append(addStyle);
-    } else {
-      var addStyle =
-        ".moveMe" +
-        numb +
-        " { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-left: 100%; animation: moveMe" +
-        numb +
-        " 26s infinite linear; position: absolute; } .moveTwo" +
-        numb +
-        "{ animation-delay: 13s; }";
-      docStyle.append(addStyle);
-    }
+  // for (var style = 1; style <= styleNum; style++) {
+  var numb = styleNum.toString();
+  if (styleNum % 2 == 1) {
+    const addStyle = replaceEach(RIGHT_STYLE, [["numb", numb]]);
+    docStyle.append(addStyle);
+  } else {
+    const addStyle = replaceEach(LEFT_STYLE, [["numb", numb]]);
+    docStyle.append(addStyle);
   }
+  // }
 }
 
 function initHTML(numOfRows) {
   var quoteBack = $("#quoteBack");
-  for (var rows = 1; rows <= numOfRows; rows++) {
-    var num = rows.toString();
-    var addDiv = "<div id='init" + num + "' class='container-fluid'></div>";
-    quoteBack.append(addDiv);
-  }
+  // for (var rows = 1; rows <= numOfRows; rows++) {
+  var num = numOfRows.toString();
+  var addDiv = "<div id='init" + num + "' class='container-fluid'></div>";
+  quoteBack.append(addDiv);
+  // }
 }
 
-function insert(initrows) {
-  initHTML(initrows);
-  initCSS(initrows);
-  init(initrows);
-}
+function insert(initrows) {}
 
 var initrows;
 if (
@@ -228,8 +203,8 @@ if (
 } else {
   initrows = 25;
 }
-insert(initrows);
-
+// insert(initrows);
+loadQuotes(heightOfPage);
 // .moveMe-right1 { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-right: 100%; animation: moveMe-right1 20s infinite linear; position: absolute; right: 10%; }
 
 // .moveTwo1-right { animation-delay: 10s; }
