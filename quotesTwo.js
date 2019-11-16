229f3738878e0e41eabfd50766f93547a11ec864
Jake Zohdi <jakez1@live.com>
2019-11-16 15:36:47 -0500

    refactered getSize

--- quotesTwo.js
+++ quotesTwo.js
@@ -31,51 +31,31 @@ const getRandomQuoteString = () => {
 const screenWidth = innerWidth;
 const screenHeight = innerHeight;
 const dimensions = [screenWidth, screenHeight];
-// console.log(innerWidth)
-// console.log(innerHeight)
 
-function getSize(leng) {
-  var fontSize = 0;
+// getSize takes in a length (leng) which will be the length of the quote strong
+// and the output is the font size that the string should be so that it does not overlap
+// with itself as the string wraps around the string but as large as it can be without doing so.
+const getSize = leng => {
+  let fontSize = 0;
+
   if (screenWidth <= 450) {
-    fontSize += Math.round(16.0 - 0.09 * leng * 0.85);
-    // fontSize = randomIntRange(8, 14)
-    var fontString = fontSize.toString();
-    // console.log(fontSize, fontString)
-    return fontString;
+    return Math.round(16.0 - 0.09 * leng * 0.85).toString();
   } else if (screenWidth <= 600 && screenWidth >= 451) {
-    fontSize += Math.round(31.0 - 0.155 * leng);
-    var fontString = fontSize.toString();
-    // console.log(fontSize, fontString)
-    return fontString;
+    return Math.round(31.0 - 0.155 * leng).toString();
   } else if (screenWidth <= 750 && screenWidth >= 601) {
-    fontSize += Math.round(35.0 - 0.15 * leng);
-    var fontString = fontSize.toString();
-    return fontString;
+    return Math.round(35.0 - 0.15 * leng).toString();
   } else if (screenWidth <= 1000 && screenWidth >= 751) {
-    fontSize = Math.round(33.0 - 0.12 * leng);
-    // fontSize = randomIntRange(12, 14)
-    var fontString = fontSize.toString();
-    // console.log(fontSize, fontString)
-    return fontString;
+    return Math.round(33.0 - 0.12 * leng).toString();
   } else if (screenWidth <= 1200 && screenWidth >= 1001) {
-    fontSize = Math.round(61.5 - 0.28 * leng);
-    var fontString = fontSize.toString();
-    return fontString;
+    return Math.round(61.5 - 0.28 * leng).toString();
   } else if (screenWidth <= 1600 && screenWidth >= 1201) {
-    fontSize = Math.round(40.0 - 0.11 * leng);
-    var fontString = fontSize.toString();
-    return fontString;
+    return Math.round(40.0 - 0.11 * leng).toString();
   } else if (screenWidth <= 2000 && screenWidth >= 1601) {
-    fontSize = Math.round(92.7 - 0.4 * leng);
-    var fontString = fontSize.toString();
-    return fontString;
+    return Math.round(91 - 0.4 * leng).toString();
   } else {
-    // To Do this function is wrong for large screens
-    fontSize = Math.round(92.7 - 0.4 * leng);
-    var fontString = fontSize.toString();
-    return fontString;
+    return Math.round(94 - 0.35 * leng).toString();
   }
-}
+};
 
 window.currentHeight = 0;
 
@@ -88,17 +68,10 @@ const MOVE_LEFT_CSS =
   ".sizerowNum { font-size: sizeToAddpx;} @keyframes moveMerowNum { 0% { transform: translate(0, " +
   "heightOnPagepx); } 100% { transform: translate(-110%, heightOnPagepx); } }";
 
-function loadQuotes(stopHeight) {
-  //   https://quotesondesign.com/api-v4-0/
-  // var quoteAPI = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=?"
-  // const totalRows = howManyRows;
-
-  // for (var currentRow = 1; currentRow <= totalRows; currentRow++)
+const loadQuotes = stopHeight => {
   while (currentHeight <= stopHeight + 20) {
     const whichDirection = Math.random() > 0.5 ? "left" : "right";
-
     const newQuoteString = getRandomQuoteString();
-
     const quoteLength = newQuoteString.length;
 
     if (quoteLength <= 188 && quoteLength >= 60) {
@@ -106,7 +79,7 @@ function loadQuotes(stopHeight) {
       initCSS(countRows, whichDirection);
       init(countRows, whichDirection);
       const sizeToAdd = getSize(quoteLength);
-      // console.log("currernt height: ", window.currentHeight);
+
       if (whichDirection == "right") {
         const cssText = replaceEach(MOVE_RIGHT_CSS, [
           ["rowNum", countRows],
@@ -115,7 +88,7 @@ function loadQuotes(stopHeight) {
         ]);
         $("#css").append(cssText);
         $("#row" + countRows).html(newQuoteString);
-        //     // console.log(countRows.toString())
+
         $("#row" + countRows + "b").html(newQuoteString);
       } else {
         const cssText = replaceEach(MOVE_LEFT_CSS, [
@@ -125,7 +98,7 @@ function loadQuotes(stopHeight) {
         ]);
         $("#css").append(cssText);
         $("#row" + countRows).html(newQuoteString);
-        //     // console.log(countRows.toString())
+
         $("#row" + countRows + "b").html(newQuoteString);
       }
       window.countRows += 1;
@@ -133,11 +106,9 @@ function loadQuotes(stopHeight) {
       window.currentHeight += parseInt(sizeToAdd);
     }
   }
-}
+};
 /*
- *
  * TO DO add init for div elements, call next init with previous init
- *
  */
 const HTML_RIGHT =
   "<div class='container-fluid'><span id='rownum' class='moveMe-rightnum sizenum'></span><span id='row" +
@@ -146,19 +117,18 @@ const HTML_LEFT =
   "<div class='container-fluid'><span id='rownum' class='moveMenum sizenum'></span><span id='row" +
   "numb' class='moveMenum  moveTwonum sizenum'></span></div>";
 
-function init(pageRow, whichDirection) {
-  // for (var rowcount = 1; rowcount <= pageRows; rowcount++) {
+const init = (pageRow, whichDirection) => {
   var num = pageRow.toString();
   var bodyDoc = $("#init" + num);
   if (whichDirection == "right") {
     const htmlForRight = replaceEach(HTML_RIGHT, [["num", num]]);
     bodyDoc.html(htmlForRight);
-  } else {
-    const htmlForLeft = replaceEach(HTML_LEFT, [["num", num]]);
-    bodyDoc.html(htmlForLeft);
+    return;
   }
-  // }
-}
+  const htmlForLeft = replaceEach(HTML_LEFT, [["num", num]]);
+  bodyDoc.html(htmlForLeft);
+};
+
 const RIGHT_STYLE =
   ".moveMe-rightnumb" +
   " { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-right: 100%; animation: moveMe-right" +
@@ -168,7 +138,7 @@ const LEFT_STYLE =
   " { margin: 0 auto; white-space: nowrap; display: inline-block; overflow: hidden; padding-left: 100%; animation: moveMe" +
   "numb seconds1s infinite linear; position: absolute; } .moveTwonumb{ animation-delay: seconds2s; }";
 
-function initCSS(styleNum, whichDirection) {
+const initCSS = (styleNum, whichDirection) => {
   var docStyle = $("#css");
   // for (var style = 1; style <= styleNum; style++) {
   var numb = styleNum.toString();
@@ -193,30 +163,15 @@ function initCSS(styleNum, whichDirection) {
     docStyle.append(addStyle);
   }
   // }
-}
+};
 
-function initHTML(numOfRows) {
-  var quoteBack = $("#quoteBack");
-  // for (var rows = 1; rows <= numOfRows; rows++) {
-  var num = numOfRows.toString();
-  var addDiv = "<div id='init" + num + "' class='container-fluid'></div>";
+const initHTML = numOfRows => {
+  const quoteBack = $("#quoteBack");
+  const num = numOfRows.toString();
+  const addDiv = "<div id='init" + num + "' class='container-fluid'></div>";
   quoteBack.append(addDiv);
-  // }
-}
+};
 
-function insert(initrows) {}
-
-var initrows;
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  initrows = 45;
-} else {
-  initrows = 25;
-}
-// insert(initrows);
 if (!window.random) {
   loadQuotes(heightOfPage);
 }
